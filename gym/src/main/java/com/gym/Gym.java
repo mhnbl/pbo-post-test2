81969e7d2ec7f948d1f048d21bbe1af29103c3b6
mhnbl <125839542+mhnbl@users.noreply.github.com>
2023-10-12 17:49:16 +0800

    Update Gym.java

--- gym/src/main/java/com/gym/Gym.java
+++ gym/src/main/java/com/gym/Gym.java
@@ -7,16 +7,37 @@ public class Gym {
     public static void main(String[] args) {
         EntitiesManager manager = new EntitiesManager();
         Scanner scanner = new Scanner(System.in);
+        
+        // Menambahkan data awal member
+        manager.addMember("Rahman", 35);
+        manager.addMember("Nabil", 19);
+        manager.addMember("Tejo", 25);
+        manager.addMember("Maulana", 17);
 
+        // Menambahkan data awal trainer
+        manager.addTrainer("Gilang", "Yoga");
+        manager.addTrainer("Budi", "Crossfit");
+        manager.addTrainer("Tara", "Weight loss");
+        manager.addTrainer("Putra", "Conditioning");
+        
+        // Menambahkan data awal equipment
+        manager.addEquipment("Treadmill", 6);
+        manager.addEquipment("Dumbbells", 50);
+        manager.addEquipment("Stationary Bike", 8);
+        manager.addEquipment("Bench Press", 10);
+        manager.addEquipment("Leg Curl", 4);
+        manager.addEquipment("Yoga Mat", 23);
+        
+        
         while (true) {
-            System.out.println("=== Gym/Fitness Management ===");
+            System.out.println("\n=== Gym/Fitness Management ===");
             System.out.println("1. Kelola Member");
             System.out.println("2. Kelola Trainer");
             System.out.println("3. Kelola Equipment");
             System.out.println("4. Keluar");
             System.out.print("Pilih menu (1-4): ");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Clear the newline character
+            scanner.nextLine(); 
 
             switch (choice) {
                 case 1:
@@ -42,7 +63,7 @@ public class Gym {
 
     private static void handleMemberMenu(EntitiesManager manager, Scanner scanner) {
         while (true) {
-            System.out.println("=== Kelola Member ===");
+            System.out.println("\n=== Kelola Member ===");
             System.out.println("1. Tambah Member");
             System.out.println("2. Edit Member");
             System.out.println("3. Hapus Member");
@@ -60,8 +81,11 @@ public class Gym {
                     System.out.print("Usia Member: ");
                     int memberAge = scanner.nextInt();
                     manager.addMember(memberName, memberAge);
+                    System.out.println("Member ditambahkan ");
                 }
                 case 2 -> {
+                    // Tampilkan Member
+                    manager.displayMembers();
                     // Edit Member
                     System.out.print("ID Member yang akan diedit: ");
                     int memberId = scanner.nextInt();
@@ -73,6 +97,8 @@ public class Gym {
                     manager.editMember(memberId, newMemberName, newMemberAge);
                 }
                 case 3 -> {
+                    // Tampilkan Member
+                    manager.displayMembers();
                     // Hapus Member
                     System.out.print("ID Member yang akan dihapus: ");
                     int memberIdToDelete = scanner.nextInt();
@@ -90,7 +116,7 @@ public class Gym {
 
     private static void handleTrainerMenu(EntitiesManager manager, Scanner scanner) {
         while (true) {
-            System.out.println("=== Kelola Trainer ===");
+            System.out.println("\n=== Kelola Trainer ===");
             System.out.println("1. Tambah Trainer");
             System.out.println("2. Edit Trainer");
             System.out.println("3. Hapus Trainer");
@@ -108,8 +134,11 @@ public class Gym {
                     System.out.print("Spesialisasi Trainer: ");
                     String trainerSpecialization = scanner.nextLine();
                     manager.addTrainer(trainerName, trainerSpecialization);
+                    System.out.println("Trainer ditambahkan ");
                 }
                 case 2 -> {
+                    // Tampilkan Trainer
+                    manager.displayTrainers();
                     // Edit Trainer
                     System.out.print("ID Trainer yang akan diedit: ");
                     int trainerId = scanner.nextInt();
@@ -121,6 +150,8 @@ public class Gym {
                     manager.editTrainer(trainerId, newTrainerName, newTrainerSpecialization);
                 }
                 case 3 -> {
+                    // Tampilkan Trainer
+                    manager.displayTrainers();
                     // Hapus Trainer
                     System.out.print("ID Trainer yang akan dihapus: ");
                     int trainerIdToDelete = scanner.nextInt();
@@ -138,7 +169,7 @@ public class Gym {
 
     private static void handleEquipmentMenu(EntitiesManager manager, Scanner scanner) {
         while (true) {
-            System.out.println("=== Kelola Equipment ===");
+            System.out.println("\n=== Kelola Equipment ===");
             System.out.println("1. Tambah Equipment");
             System.out.println("2. Edit Equipment");
             System.out.println("3. Hapus Equipment");
@@ -156,8 +187,11 @@ public class Gym {
                     System.out.print("Jumlah Equipment: ");
                     int equipmentCount = scanner.nextInt();
                     manager.addEquipment(equipmentName, equipmentCount);
+                    System.out.println("Equipment ditambahkan ");
                 }
                 case 2 -> {
+                    // Tampilkan Equipment
+                    manager.displayEquipment();
                     // Edit Equipment
                     System.out.print("ID Equipment yang akan diedit: ");
                     int equipmentId = scanner.nextInt();
@@ -169,6 +203,8 @@ public class Gym {
                     manager.editEquipment(equipmentId, newEquipmentName, newEquipmentCount);
                 }
                 case 3 -> {
+                    // Tampilkan Equipment
+                    manager.displayEquipment();
                     // Hapus Equipment
                     System.out.print("ID Equipment yang akan dihapus: ");
                     int equipmentIdToDelete = scanner.nextInt();
